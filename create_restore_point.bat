592a7373db121e85260d090b65a90f06d47fba1f
solbadguy <solbadguy90210@gmail.com>
2021-03-03 10:37:26 +0300

    123

--- create_restore_point.bat
+++ create_restore_point.bat
@@ -1,5 +1,5 @@
 echo Start create restore point
 
-wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "<RestorePointName>", 100, 7
+wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "<Clear install>", 100, 7
 
 echo Done!
